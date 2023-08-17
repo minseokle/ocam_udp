1e309b67ee87e90cb9831cac521b9be0aea98855
minseokle <0311ben@naver.com>
2023-08-17 23:20:58 +0900

    feat : update node

--- main.cpp
+++ main.cpp
@@ -13,12 +13,21 @@
  *  - Press the key '-' for decrease the brightness.
  */
 
+#include <arpa/inet.h>
 #include <errno.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include "opencv2/opencv.hpp"
 #include "withrobot_camera.hpp" /* withrobot camera API */
+
+#define PORT 20162
+#define BUFFER_SIZE 4096  // 서버에 이용
+#define BUFF_SIZE 100     // 클라이언트에 이용
 
 /*
  *	Main
@@ -130,6 +139,46 @@ int main(int argc, char* argv[]) {
   // cv::namedWindow(windowName.c_str(),
   // CV_WINDOW_KEEPRATIO|CV_WINDOW_AUTOSIZE);
 
+  // udp set-------------------
+  struct sockaddr_in serverAddress, clientAddress;
+  int server_fd, client_fd;
+  int client_addr_size;
+  ssize_t receivedBytes;
+  ssize_t sentBytes;
+
+  socklen_t clientAddressLength = 0;
+
+  memset(&serverAddress, 0, sizeof(serverAddress));
+  memset(&clientAddress, 0, sizeof(clientAddress));
+
+  clientAddress.sin_family = AF_INET;
+  inet_aton("192.168.1.151", (struct in_addr*)&clientAddress.sin_addr.s_addr);
+  clientAddress.sin_port = htons(PORT);
+
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+  serverAddress.sin_port = htons(PORT);
+
+  // 서버 소켓 생성 및 서버 주소와 bind
+
+  // 서버 소켓 생성(UDP니 SOCK_DGRAM이용)
+  if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)  // SOCK_DGRAM : UDP
+  {
+    printf("Sever : can not Open Socket\n");
+    exit(0);
+  }
+
+  // bind 과정
+  if (bind(server_fd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) <
+      0) {
+    printf("Server : can not bind local address");
+    exit(0);
+  }
+
+  printf("Server: waiting connection request.\n");
+
+  //-------------------------------------
+
   /*
    * Main loop
    */
@@ -152,6 +201,19 @@ int main(int argc, char* argv[]) {
     /* Show image */
     cv::imshow(windowName.c_str(), colorImg);
 
+
+    //(1) jpeg compression
+    std::vector<uchar> buff;  // buffer for coding
+    std::vector<int> param(2);
+    
+    param[0] = cv::IMWRITE_JPEG_QUALITY;
+    param[1] = 95;  // default(95) 0-100
+
+    cv::imencode(".jpg", colorImg, buff, param);
+
+    sentBytes = sendto(server_fd, buff.data(), buff.size(), 0,
+                       (struct sockaddr*)&clientAddress, sizeof(clientAddress));
+    std::cout<<"send\n";
     char key = cv::waitKey(1);
 
     /* Keyboard options */
@@ -196,6 +258,9 @@ int main(int argc, char* argv[]) {
    * Stop streaming
    */
   camera.stop();
+
+  // 소켓 close
+  close(server_fd);
 
   printf("Done.\n");
 
